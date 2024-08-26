6ec185acfed9102da3344bdff8a1a587f76983c1
InspectorBoat <116048347+InspectorBoat@users.noreply.github.com>
2024-08-26 05:02:17 -0400

    renamed PackedNibbleArray .raw to .bytes

--- util/packed_nibble_array.zig
+++ util/packed_nibble_array.zig
@@ -2,13 +2,13 @@ const std = @import("std");
 
 pub fn PackedNibbleArray(len: usize) type {
     return struct {
-        raw: [len / 2]u8,
+        bytes: [len / 2]u8,
         pub fn get(self: *const @This(), index: usize) u4 {
-            return @truncate(self.raw[index / 2] >> @as(u3, @intCast((index & 1) * 4)));
+            return @truncate(self.bytes[index / 2] >> @as(u3, @intCast((index & 1) * 4)));
         }
         pub fn set(self: *@This(), index: usize, value: u4) void {
-            self.raw[index / 2] &= @as(u8, 0xF) << @intCast(((index & 1) ^ 1) * 4);
-            self.raw[index / 2] |= @as(u8, value) << @intCast((index & 1) * 4);
+            self.bytes[index / 2] &= @as(u8, 0xF) << @intCast(((index & 1) ^ 1) * 4);
+            self.bytes[index / 2] |= @as(u8, value) << @intCast((index & 1) * 4);
         }
         pub fn init(values: [len]u4) @This() {
             var self: @This() = .{ .raw = .{0} ** (len / 2) };
