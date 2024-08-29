190fa1059daef6987350baf44ea42575703c2c6e
InspectorBoat <116048347+InspectorBoat@users.noreply.github.com>
2024-08-28 20:17:17 -0400

    Implement chat message debugging

--- main/network/packet/s2c/play/ChatMessageS2CPacket.zig
+++ main/network/packet/s2c/play/ChatMessageS2CPacket.zig
@@ -3,18 +3,19 @@ const Game = @import("../../../../game.zig").Game;
 const ReadPacketBuffer = @import("../../../../network/packet/ReadPacketBuffer.zig");
 
 message: []const u8,
-type: u8,
+type: i8,
 
 comptime handle_on_network_thread: bool = false,
 
 pub fn decode(buffer: *ReadPacketBuffer, allocator: std.mem.Allocator) !@This() {
-    _ = allocator;
-    _ = buffer;
-    return undefined;
+    return .{
+        .message = try buffer.readStringAllocating(32767, allocator),
+        .type = try buffer.read(i8),
+    };
 }
 
 pub fn handleOnMainThread(self: *@This(), game: *Game, allocator: std.mem.Allocator) !void {
+    std.debug.print("message: {s} type: {}", .{ self.message, self.type });
     _ = allocator;
     _ = game;
-    _ = self;
 }
